4ae23b9ec1d9bce1d845c603fb4b17cf359390ca
Nandini Breggin <bregginnr@s.dcsdk12.org>
2021-01-03 12:18:30 -0700

    Activity4

---
+++ answers.md
@@ -0,0 +1,14 @@
+* What would it mean to build a robot?
+* What would it mean to understand french?
+* What makes you think that I like you?
+* What makes you think that I confuse you?
+
+
+A example of when this structure wouldn't work well, is with a sentence asking for reason. The code/bot doesn't have enough information to respond to the reasoning with more reason. The response given doesn't make sense.
+
+*Ex:* Why does the Earth rotate around the sun?
+* Response: You don't say.
+
+You could improve it by making the bot more aware. 
+
+You could improve it by make the bot more "aware." However, that would make the code more complicated. It need to look for the 'why' keyword, so it can respond accordingly with different responses based on the type of question.
--- src/Magpie4.java
+++ src/Magpie4.java
@@ -46,6 +46,13 @@ public class Magpie4 {
 		// Responses which require transformations
 		else if (findKeyword(statement, "I want to", 0) >= 0) {
 			response = transformIWantToStatement(statement);
+
+		} else if (findKeyword(statement, "I want", 0) >= 0) {
+			response = transformIWantStatement(statement);
+
+		} else if (findKeyword(statement, "I", 0) >= 0) {
+			response = transformIYouStatement(statement);
+
 		}
 
 		else {
@@ -82,6 +89,18 @@ public class Magpie4 {
 		return "What would it mean to " + restOfStatement + "?";
 	}
 
+	private String transformIWantStatement(String statement) {
+		// Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals(".")) {
+			statement = statement.substring(0, statement.length() - 1);
+		}
+		int psn = findKeyword(statement, "I want", 0);
+		String restOfStatement = statement.substring(psn + 6).trim();
+		return "Would you really be happy if you had " + restOfStatement + "?";
+	}
+
 	/**
 	 * Take a statement with "you <something> me" and transform it into
 	 * "What makes you think that I <something> you?"
@@ -104,6 +123,22 @@ public class Magpie4 {
 		String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe)
 				.trim();
 		return "What makes you think that I " + restOfStatement + " you?";
+	}
+
+	private String transformIYouStatement(String statement) {
+		// Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement.length() - 1);
+		if (lastChar.equals(".")) {
+			statement = statement.substring(0, statement.length() - 1);
+		}
+
+		int psnOfYou = findKeyword(statement, "I", 0);
+		int psnOfMe = findKeyword(statement, "you", psnOfYou + 1);
+
+		String restOfStatement = statement.substring(psnOfYou + 1, psnOfMe)
+				.trim();
+		return "Why do you " + restOfStatement + " me?";
 	}
 
 	/**
